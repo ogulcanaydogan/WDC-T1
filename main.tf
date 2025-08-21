65a792ad350985b4a10daf47d043cf40117fc7e4
Ogulcan Aydogan <72593834+ogulcanaydogan@users.noreply.github.com>
2025-08-21 14:30:44 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -162,16 +162,3 @@ resource "aws_instance" "coolify" {
     volume_type = "gp3"
   }
 }
-
-# --- Outputs ---
-output "public_ip" {
-  value = aws_instance.coolify.public_ip
-}
-
-output "ssh_command" {
-  value = "ssh -i ~/.ssh/${var.key_name}.pem ubuntu@${aws_instance.coolify.public_ip}"
-}
-
-output "coolify_url" {
-  value = "http://${aws_instance.coolify.public_ip}"
-}
