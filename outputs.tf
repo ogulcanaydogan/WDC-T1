9c8f81d9dd05653e08d591d010033d1d99c15373
Ogulcan Aydogan <72593834+ogulcanaydogan@users.noreply.github.com>
2025-08-21 14:31:04 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
-# output "public_ip" {
-#   value = aws_instance.coolify.public_ip
-# }
+# --- Outputs ---
+output "public_ip" {
+  value = aws_instance.coolify.public_ip
+}
 
-# output "ssh_command" {
-#   value = "ssh -i ~/.ssh/${var.key_name}.pem ubuntu@${aws_instance.coolify.public_ip}"
-# }
+output "ssh_command" {
+  value = "ssh -i ~/.ssh/${var.key_name}.pem ubuntu@${aws_instance.coolify.public_ip}"
+}
 
-# # Coolify serves via Traefik on :80 (and later :443 after SSL)
-# output "coolify_url" {
-#   value = "http://${aws_instance.coolify.public_ip}"
-# }
+output "coolify_url" {
+  value = "http://${aws_instance.coolify.public_ip}"
+}
